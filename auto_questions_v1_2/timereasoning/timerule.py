3be4e8e04ab184658ba9454e71f523be8d307ca4
hezonglianheng <976410976@qq.com>
2024-09-05 20:07:39 +0800

    参照基础类型的定义方式定义时间领域的专属情况

--- auto_questions_v1_2/timereasoning/timerule.py
+++ auto_questions_v1_2/timereasoning/timerule.py
@@ -14,89 +14,170 @@ from typing import Optional
 # 将上级目录加入到sys.path中
 sys.path.append(Path(__file__).resolve().parents[1].as_posix())
 
+from proposition.prop import DoubleProp, SingleProp
 import proposition.rule as rule
 import timereasoning.timeprop as timeprop
 
-class TimeRule(rule.Rule):
+# 从两个单元素命题推出一个双元素命题的规则
+
+class GetBeforeTimeP(rule.TwoSingleToDoubleRule):
+    _rule_tuple = (timeprop.TemporalP, timeprop.TemporalP, timeprop.BeforeTimeP)
+
     @classmethod
-    @abc.abstractmethod
-    def reason(cls, prop1: timeprop.TimeP, prop2: timeprop.TimeP) -> Optional[timeprop.TimeP]:
-        """根据时间命题推理生成新的时间命题
-
-        Args:
-            prop1 (TimeP): 时间命题1
-            prop2 (TimeP): 时间命题2
-
-        Returns:
-            TimeP: 新的时间命题
-        """
-        pass
-
-class Single2DoubleR(TimeRule):
-    """
-    从两个单元素命题推出一个双元素命题的规则\n
-    推导方式: F(x) && F(y) -> G(x, y)
-    """
-    @classmethod
-    def reason(cls, prop1: timeprop.SingleTimeP, prop2: timeprop.SingleTimeP) -> timeprop.TimeP | None:
-        if not all([isinstance(i, timeprop.SingleTimeP) for i in [prop1, prop2]]):
+    def reason(cls, prop1: timeprop.TemporalP, prop2: timeprop.TemporalP) -> timeprop.BeforeTimeP | None:
+        if not cls._assert_condition(prop1, prop2):
             return None
-        if prop1 == prop2:
-            return None
-        return timeprop.DoubleTimeP.build(prop1.element, prop2.element)
-
-class TransitivityR(TimeRule):
-    """
-    传递性推理规则\n
-    推导方式: F(x, y) && F(y, z) -> F(x, z)
-    """
-    tps: type[timeprop.DoubleTimeP] = [
-        timeprop.BeforeP,
-        timeprop.BeforeTimeP,
-        timeprop.AfterP,
-        timeprop.AfterTimeP,
-        timeprop.SimultaneousP,
-        timeprop.LongP,
-        timeprop.LongTimeP,
-        timeprop.ShortP,
-        timeprop.ShortTimeP,
-        timeprop.SameLenTimeP,
-    ]
-
-    @classmethod
-    def add_tps(cls, *tps: type[timeprop.DoubleTimeP]):
-        """添加新的有传递性的时间命题类型"""
-        cls.tps += tps
-    
-    @classmethod
-    def reason(cls, prop1: timeprop.TimeP, prop2: timeprop.TimeP) -> timeprop.TimeP | None:
-        if type(prop1) != type(prop2):
-            return None
-        for tp in cls.tps:
-            if isinstance(prop1, tp) and prop1.prev_element == prop2.new_element and prop1.new_element != prop2.prev_element:
-                return tp(prop1.new_element, prop2.prev_element)
-        return None
-
-class SingleDouble2SingleR(TimeRule):
-    """
-    一单元素命题和一双元素命题推一单元素命题\n
-    推导方式: \n
-    F(x) && G(x, y) -> F(y)\n
-    F(y) && G(x, y) -> F(x)
-    """
-    @classmethod
-    def reason(cls, prop1: timeprop.SingleTimeP, prop2: timeprop.DoubleTimeP) -> timeprop.TimeP | None:
-        if not (isinstance(prop1, timeprop.TemporalP) and isinstance(prop2, timeprop.DoubleTimeP)):
-            return None # 如果prop1不是单元素时间命题或者prop2不是双元素时间命题，则返回None
-        if not prop2.precise:
-            return None # 如果prop2非精确，则无法进行推导
+        elif prop1.time < prop2.time:
+            return super().reason(prop1, prop2)
         else:
-            if prop1.element == prop2.prev_element:
-                return timeprop.SingleTimeP.build(prop2.new_element)
-            elif prop1.element == prop2.new_element:
-                return timeprop.SingleTimeP.build(prop2.prev_element)
-            else:
-                return None
+            return None
+
+class GetAfterTimeP(rule.TwoSingleToDoubleRule):
+    _rule_tuple = (timeprop.TemporalP, timeprop.TemporalP, timeprop.AfterTimeP)
+
+    @classmethod
+    def reason(cls, prop1: timeprop.TemporalP, prop2: timeprop.TemporalP) -> timeprop.AfterTimeP | None:
+        if not cls._assert_condition(prop1, prop2):
+            return None
+        elif prop1.time > prop2.time:
+            return super().reason(prop1, prop2)
+        else:
+            return None
+
+class GetSimultaneousP(rule.TwoSingleToDoubleRule):
+    _rule_tuple = (timeprop.TemporalP, timeprop.TemporalP, timeprop.SimultaneousP)
+
+    @classmethod
+    def reason(cls, prop1: timeprop.TemporalP, prop2: timeprop.TemporalP) -> timeprop.SimultaneousP | None:
+        if not cls._assert_condition(prop1, prop2):
+            return None
+        elif prop1.time == prop2.time:
+            return super().reason(prop1, prop2)
+        else:
+            return None
+
+class GetLongTimeP(rule.TwoSingleToDoubleRule):
+    _rule_tuple = (timeprop.DurationP, timeprop.DurationP, timeprop.LongTimeP)
+
+    @classmethod
+    def reason(cls, prop1: timeprop.DurationP, prop2: timeprop.DurationP) -> timeprop.LongTimeP | None:
+        if not cls._assert_condition(prop1, prop2):
+            return None
+        elif prop1.duration > prop2.duration:
+            return super().reason(prop1, prop2)
+        else:
+            return None
+
+class GetShortTimeP(rule.TwoSingleToDoubleRule):
+    _rule_tuple = (timeprop.DurationP, timeprop.DurationP, timeprop.ShortTimeP)
+
+    @classmethod
+    def reason(cls, prop1: timeprop.DurationP, prop2: timeprop.DurationP) -> timeprop.ShortTimeP | None:
+        if not cls._assert_condition(prop1, prop2):
+            return None
+        elif prop1.duration < prop2.duration:
+            return super().reason(prop1, prop2)
+        else:
+            return None
+
+class GetSameLenTimeP(rule.TwoSingleToDoubleRule):
+    _rule_tuple = (timeprop.DurationP, timeprop.DurationP, timeprop.SameLenTimeP)
+
+    @classmethod
+    def reason(cls, prop1: timeprop.DurationP, prop2: timeprop.DurationP) -> timeprop.SameLenTimeP | None:
+        if not cls._assert_condition(prop1, prop2):
+            return None
+        elif prop1.duration == prop2.duration:
+            return super().reason(prop1, prop2)
+        else:
+            return None
+
+# 传递性规则
+
+class BeforeTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.BeforeP, timeprop.BeforeP, timeprop.BeforeP)
+
+class BeforeTimeTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.BeforeTimeP, timeprop.BeforeTimeP, timeprop.BeforeTimeP)
+
+class AfterTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.AfterP, timeprop.AfterP, timeprop.AfterP)
+
+class AfterTimeTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.AfterTimeP, timeprop.AfterTimeP, timeprop.AfterTimeP)
+
+class SimultaneousTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.SimultaneousP, timeprop.SimultaneousP, timeprop.SimultaneousP)
+
+class LongTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.LongP, timeprop.LongP, timeprop.LongP)
+
+class LongTimeTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.LongTimeP, timeprop.LongTimeP, timeprop.LongTimeP)
+
+class ShortTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.ShortP, timeprop.ShortP, timeprop.ShortP)
+
+class ShortTimeTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.ShortTimeP, timeprop.ShortTimeP, timeprop.ShortTimeP)
+
+class SameLenTrans(rule.TransitivityRule):
+    _rule_tuple = (timeprop.SameLenTimeP, timeprop.SameLenTimeP, timeprop.SameLenTimeP)
+
+# 一单元素命题和一双元素命题推一单元素命题
+
+class FromDoubleTime(rule.DoubleSingleToSingleRule):
+    @classmethod
+    def reason(cls, prop1: DoubleProp, prop2: SingleProp) -> SingleProp | None:
+        if not cls._assert_condition(prop1, prop2):
+            return None
+        if prop1.element1 == prop2.element:
+            return timeprop.SingleTimeP.build(prop1.element2)
+        elif prop1.element2 == prop2.element:
+            return timeprop.SingleTimeP.build(prop1.element1)
+        else:
+            return None
+
+class FromBeforeTime(FromDoubleTime):
+    _rule_tuple = (timeprop.BeforeTimeP, timeprop.SingleTimeP, timeprop.SingleTimeP)
+
+class FromAfterTime(FromDoubleTime):
+    _rule_tuple = (timeprop.AfterTimeP, timeprop.SingleTimeP, timeprop.SingleTimeP)
+
+class FromSimultaneous(FromDoubleTime):
+    _rule_tuple = (timeprop.SimultaneousP, timeprop.SingleTimeP, timeprop.SingleTimeP)
+
+class FromLongTime(FromDoubleTime):
+    _rule_tuple = (timeprop.LongTimeP, timeprop.SingleTimeP, timeprop.SingleTimeP)
+
+class FromShortTime(FromDoubleTime):
+    _rule_tuple = (timeprop.ShortTimeP, timeprop.SingleTimeP, timeprop.SingleTimeP)
+
+class FromSameLenTime(FromDoubleTime):
+    _rule_tuple = (timeprop.SameLenTimeP, timeprop.SingleTimeP, timeprop.SingleTimeP)
 
 # 时间推理规则列表
-RULES: list[type[TimeRule]] = [Single2DoubleR, TransitivityR, SingleDouble2SingleR]
+RULES: list[type[rule.Rule]] = [
+    GetBeforeTimeP,
+    GetAfterTimeP,
+    GetSimultaneousP,
+    GetLongTimeP,
+    GetShortTimeP,
+    GetSameLenTimeP,
+    BeforeTrans,
+    BeforeTimeTrans,
+    AfterTrans,
+    AfterTimeTrans,
+    SimultaneousTrans,
+    LongTrans,
+    LongTimeTrans,
+    ShortTrans,
+    ShortTimeTrans,
+    SameLenTrans,
+    FromBeforeTime,
+    FromAfterTime,
+    FromSimultaneous,
+    FromLongTime,
+    FromShortTime,
+    FromSameLenTime,
+]
