56e905f6a8caabc980d50068cfbb97c5bdf96c19
hezonglianheng <976410976@qq.com>
2024-12-01 00:21:28 +0800

    更新level函数超参数和算法

--- auto_questions_v1_2/proposition/level.py
+++ auto_questions_v1_2/proposition/level.py
@@ -7,13 +7,13 @@
 
 # weights of parameters.
 # 链长
-chain_length_weight = 0.05
+chain_length_weight = 0.01
 # 命题长度
-statement_length_weight = 0.05
+statement_length_weight = 0.01
 # 选项数
-option_num_weight = 0.5
+option_num_weight = 0.2
 # 知识数量
-knowledge_num_weight = 0.3
+knowledge_num_weight = 0.1
 
 # functions.
 def ask_level(chain_len: int, statement_len: int, option_num: int, knowledge_num: int, scene_level: float) -> int:
@@ -34,4 +34,10 @@ def ask_level(chain_len: int, statement_len: int, option_num: int, knowledge_num
     option_level = option_num * option_num_weight
     knowledge_level = knowledge_num * knowledge_num_weight
     level = chain_level + statement_level + option_level + knowledge_level + scene_level
-    return round(level)
+    level_rank =  round(level)
+    if level_rank < 1:
+        return 1
+    elif level_rank > 4:
+        return 4
+    else:
+        return level_rank
