8e1847d3418bd990dbff728b4c79e92e78d3f417
hezonglianheng <976410976@qq.com>
2025-02-20 18:58:23 +0800

    新建V3程序全局配置文件

---
+++ auto_questions_v1_3/config.py
@@ -0,0 +1,21 @@
+# encoding: utf8
+# date: 2025-01-16
+
+"""程序的全局配置文件
+"""
+
+from pathlib import Path
+
+# 语言设置
+CHINESE = "cn"
+ENGLISH = "en"
+
+# Penn-Treebank 词性标签
+PAST_TENSE = "VBD"
+PAST_PARTICIPLE = "VBN"
+PRESENT_PARTICIPLE = "VBG"
+PLURAL_NOUN = "NNS"
+
+# 文件配置
+KNOWLEDGE_BASE_DIR = Path(__file__).parent / "knowledge_base"
+TIME_UNIT_FILE = KNOWLEDGE_BASE_DIR / "time_unit.json5"
