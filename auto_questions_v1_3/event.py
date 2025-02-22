9b423021736732e6e46aa93c848f1397d4781921
hezonglianheng <976410976@qq.com>
2025-02-22 15:42:50 +0800

    设置自定义事件的翻译方式

--- auto_questions_v1_3/event.py
+++ auto_questions_v1_3/event.py
@@ -15,10 +15,16 @@ PRONOUN = "pronoun" # 代词
 SUBJECT = "subject" # 主语
 PREDICATE = "predicate" # 谓语
 OBJECT = "object" # 宾语
+TENSE = "tense" # 时态
+PAST = "past" # 过去时
+PRESENT = "present" # 现在时
+FUTURE = "future" # 将来时
+IS_THIRD_SINGULAR = "is_third_singular" # 是否第三人称单数
 
 class MyObject(element.Element):
     """自定义的事件中的事物元素，一般作为事件的主语
     """
+
     def __init__(self, name = "", kind = "", **kwargs):
         super().__init__(name, kind, **kwargs)
         self.use_pronoun: bool = False # 是否使用代词
@@ -39,7 +45,27 @@ class MyObject(element.Element):
             return curr_info[NAME]
 
 class Event(element.Element):
-    pass
+    """自定义的事件元素
+    """
+
+    def translate(self, lang) -> str:
+        subject_element: MyObject = self[SUBJECT]
+        subject: str = subject_element.translate(lang)
+        predicate: str = lemminflect.getInflection(self[PREDICATE][lang], tag = config.VERB_BASE_FORM)[0]
+        obj: str = self[OBJECT][lang]
+        if lang == config.CHINESE:
+            return config.SEPARATE[lang].join([subject, predicate, obj])
+        elif lang == config.ENGLISH:
+            if self[TENSE] == PAST:
+                predicate = lemminflect.getInflection(predicate, tag = config.PAST_TENSE)[0]
+            elif self[TENSE] == FUTURE:
+                predicate = "will " + predicate
+            else:
+                if subject_element[IS_THIRD_SINGULAR]:
+                    predicate = lemminflect.getInflection(predicate, tag = config.THIRD_PERSON_SINGULAR_PRESENT)[0]
+            return config.SEPARATE[lang].join([subject, predicate, obj])
+        else:
+            raise ValueError(f"不支持的翻译语言: {lang}")
 
 if __name__ == "__main__":
     # 测试
@@ -53,9 +79,15 @@ if __name__ == "__main__":
             PRONOUN: "he",
         }
     }
-    obj = MyObject(name = "Tom", kind = "person", name_info = nameinfo)
-    print(obj.translate(config.CHINESE))
-    print(obj.translate(config.ENGLISH))
-    obj.use_pronoun = True
-    print(obj.translate(config.CHINESE))
-    print(obj.translate(config.ENGLISH))
+    subject = MyObject(name = "Tom", kind = "person", is_third_singular = True, name_info = nameinfo, )
+    print(subject.translate(config.CHINESE))
+    print(subject.translate(config.ENGLISH))
+    subject.use_pronoun = True
+    print(subject.translate(config.CHINESE))
+    print(subject.translate(config.ENGLISH))
+    subject.use_pronoun = False
+    event = {"cn": "打", "en": "play"}
+    obj = {"cn": "羽毛球", "en": "badminton"}
+    e = Event(subject = subject, predicate = event, object = obj, tense = PRESENT)
+    print(e.translate(config.CHINESE))
+    print(e.translate(config.ENGLISH))
