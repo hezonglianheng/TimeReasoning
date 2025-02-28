eb50b4acafd34833f75b51dfef8a1fbb5314c0a8
hezonglianheng <976410976@qq.com>
2025-02-28 20:46:00 +0800

    调整命题模板文件的路径位置，将不同时间单位的时间模板分离

--- auto_questions_v1_3/config.py
+++ auto_questions_v1_3/config.py
@@ -23,7 +23,20 @@ THIRD_PERSON_SINGULAR_PRESENT = "VBZ"
 VERB_BASE_FORM = "VB"
 PLURAL_NOUN = "NNS"
 
+# 当前运行配置，需要在运行时修改
+CURR_UNIT = ""
+
+def set_curr_unit(unit: str):
+    """设置当前时间单位
+
+    Args:
+        unit (str): 时间单位
+    """
+    global CURR_UNIT
+    CURR_UNIT = unit
+
 # 文件配置
 KNOWLEDGE_BASE_DIR = Path(__file__).parent / "knowledge_base"
 TIME_UNIT_FILE = KNOWLEDGE_BASE_DIR / "time_unit.json5"
-PROP_FILE = KNOWLEDGE_BASE_DIR / "proposition.json5"
+PROP_DIR = KNOWLEDGE_BASE_DIR / "proposition"
+PROP_FILE = PROP_DIR / f"{CURR_UNIT}.json5"
